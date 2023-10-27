22a5818ed9f6cbd8705c009a888f5e62fb28995e
Dennis Traub <traubd@amazon.de>
2023-10-27 12:32:15 +0200

    Add empty infrastructure stack

---
+++ infrastructure/src/main/java/aws/community/examples/InfrastructureStack.java
@@ -0,0 +1,17 @@
+package aws.community.examples;
+
+import software.amazon.awscdk.App;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.constructs.Construct;
+
+public class InfrastructureStack extends Stack {
+    public InfrastructureStack(final App parent, final String id) {
+        this(parent, id, null);
+    }
+
+    public InfrastructureStack(final Construct parent, final String id, final StackProps props) {
+        super(parent, id, props);
+
+    }
+}
